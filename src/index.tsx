3696c951bac9cc5654a4af51b4f605d1237ddd59
illicchpv <illich58@mail.ru>
2025-10-22 17:56:25 +0300

    обернули приложение в Provider

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { router } from './router';
 
+import { store } from './store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
