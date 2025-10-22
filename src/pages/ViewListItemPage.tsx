251e8b1ffead5dd17a97d5940db05be06319a2c5
illicchpv <illich58@mail.ru>
2025-10-22 19:28:51 +0300

    todos берём из stor

--- src/pages/ViewListItemPage.tsx
+++ src/pages/ViewListItemPage.tsx
@@ -2,18 +2,21 @@ import { useEffect, useState } from "react";
 import { ToDo } from "../Models/todo-item";
 import { useNavigate, useParams } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 interface ComponentProps {
   todos: ToDo[];
 }
 
-export const ViewListItemPage = ({ todos }: ComponentProps) => {
+export const ViewListItemPage = () => {
+  const todoList = useSelector((state: RootState) => state.todoList.todos)
   const { id } = useParams()
   const navigate = useNavigate()
   const [todo, setTodo] = useState<ToDo | null>(null)
 
   useEffect(() => {
-    const searchToDo = todos.find(el => el.id === Number(id))
+    const searchToDo = todoList.find(el => el.id === Number(id))
     console.log('searchToDo: ', searchToDo);
     if (!searchToDo) {
       console.log(`Задача id:${id} не найдена!`)
@@ -21,7 +24,7 @@ export const ViewListItemPage = ({ todos }: ComponentProps) => {
     } else {
       setTodo(searchToDo)
     }
-  }, [todos, id, navigate])
+  }, [todoList, id, navigate])
 
   if(!todo) return null
   const str = todo.text
