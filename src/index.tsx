7d7151e7bc4083979b26fda3c666818745a1f671
illicchpv <illich58@mail.ru>
2025-10-22 17:19:32 +0300

    перенос из index.tsx router и todos в router.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,10 @@
 import './assets/scss/normalize.scss'
 import './assets/scss/style.scss'
+import { RouterProvider } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ToDoListPage } from './pages/ToDoListPage';
-import { BrowserRouter, createBrowserRouter, Route, Router, RouterProvider, Routes } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { Header } from './components/Header/Header';
-import { ToDo } from './Models/todo-item';
-import { NotFoundPage } from './pages/404';
-import { ItemPage } from './pages/ItemPage';
-import { Layout } from './layouts/Layout';
+import { router } from './router';
 
-const todos: ToDo[] = [
-  {
-    id: 0,
-    text: 'Первая задача(изменить нельзя)',
-    isDone: false,
-  },
-  {
-    id: 1,
-    text: 'Вторая задача',
-    isDone: true,
-  },
-  {
-    id: 2,
-    text: 'Третья задача',
-    isDone: false,
-  },
-  {
-    id: 3,
-    text: 'Четвёртая задача',
-    isDone: true,
-  },
-]
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage todos={todos} />,
-      },
-      {
-        path: "/todo",
-        element: <ToDoListPage />,
-      },
-      {
-        path: "/list/:id",
-        element: <ItemPage todos={todos} />,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <NotFoundPage />,
-  },
-], { basename: '/' }) // { basename: '/app/' }
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -66,16 +12,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    {/* <BrowserRouter>
-      <Header />
-
-      <Routes>
-        <Route path="/" element={<HomePage todos={todos} />} />
-        <Route path="/todo" element={<ToDoListPage />} />
-        <Route path="/list/:id" element={<ItemPage todos={todos}/>} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter> */}
   </React.StrictMode>
 );
 
---
+++ src/router.tsx
@@ -0,0 +1,56 @@
+import { createBrowserRouter } from "react-router-dom"
+import { Layout } from "./layouts/Layout"
+import { NotFoundPage } from "./pages/404"
+import { HomePage } from "./pages/HomePage"
+import { ToDoListPage } from "./pages/ToDoListPage"
+import { ItemPage } from "./pages/ItemPage"
+import { ToDo } from "./Models/todo-item"
+
+const todos: ToDo[] = [
+  {
+    id: 0,
+    text: 'Первая задача(изменить нельзя)',
+    isDone: false,
+  },
+  {
+    id: 1,
+    text: 'Вторая задача',
+    isDone: true,
+  },
+  {
+    id: 2,
+    text: 'Третья задача',
+    isDone: false,
+  },
+  {
+    id: 3,
+    text: 'Четвёртая задача',
+    isDone: true,
+  },
+]
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage todos={todos} />,
+      },
+      {
+        path: "/todo",
+        element: <ToDoListPage />,
+      },
+      {
+        path: "/list/:id",
+        element: <ItemPage todos={todos} />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+], { basename: '/' }) // { basename: '/app/' }
