4b552e84f5c6b68aec83ab935f246c265cc955a9
illicchpv <illich58@mail.ru>
2025-10-22 18:16:12 +0300

    создали заготовку todoSlice

---
+++ src/features/todoList.ts
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import { ToDo } from '../Models/todo-item'
+
+export interface TodoState {
+  todo: ToDo[]
+}
+
+const initialState: TodoState = {
+  todo: [],
+}
+
+export const todoSlice = createSlice({  // reducer
+  name: 'todo',
+  initialState,
+  reducers: { // мутаторы для изменения состояния
+    createAction: (state, action: PayloadAction<ToDo>) => {
+      // state.todo.push(action.payload)
+      console.log('createAction.payload: ', action.payload);
+    },
+    updateAction: (state, action: PayloadAction<ToDo>) => {
+      console.log('updateAction.payload: ', action.payload);
+    },
+    deleteAction: (state, action: PayloadAction<ToDo>) => {
+      console.log('deleteAction.payload: ', action.payload);
+    },
+    // increment: (state) => {
+    //   // Redux Toolkit allows us to write "mutating" logic in reducers. It
+    //   // doesn't actually mutate the state because it uses the Immer library,
+    //   // which detects changes to a "draft state" and produces a brand new
+    //   // immutable state based off those changes
+    //   state.value += 1
+    // },
+    // decrement: (state) => {
+    //   state.value -= 1
+    // },
+    // incrementByAmount: (state, action: PayloadAction<number>) => {
+    //   state.value += action.payload
+    // },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const {
+  createAction,
+  updateAction,
+  deleteAction
+} = todoSlice.actions
+
+export default todoSlice.reducer
